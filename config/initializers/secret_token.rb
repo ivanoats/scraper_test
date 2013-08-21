8dcf068f84f1bafed7d7cabec4f4f2d311a1e65c
Ivan Storck <ivanoats@gmail.com>
2013-08-21 12:55:38 -0700

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScraperTest::Application.config.secret_key_base = '272d6a9a0cd9fb42b209a1b086742485ffed305aad97754700b70c01f69ad1b7492ff81aab1a0282ad1264365f152aa381272f4e54cdfe25cd0c5fdeef08b59e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScraperTest::Application.config.session_store :cookie_store, key: '_scraper_test_session'
